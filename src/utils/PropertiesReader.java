04a18b49c1efaf938e8f8af9f47c599d37324f9d
王彦超 <wang_yc@suixingpay.com>
2017-08-01 19:13:58 +0800

    no message

--- src/utils/PropertiesReader.java
+++ src/utils/PropertiesReader.java
@@ -12,29 +12,29 @@ import java.util.Properties;
  */
 public class PropertiesReader {
 
-    private static Map<String, String> instance;
+    private static Map<String, String> instance = new PropertiesReader().getProperties();
 
     private PropertiesReader() {
     }
 
     public static String getInstance(String name) throws IOException {
-        if (instance == null) {
-            instance = new PropertiesReader().getProperties();
-        }
         return instance.get(name);
     }
 
-    private Map<String, String> getProperties() throws IOException {
+    private Map<String, String> getProperties() {
         Properties props = new Properties();
         Map<String, String> map = new HashMap<String, String>();
         InputStream in = PropertiesReader.class.getClassLoader().getResourceAsStream("Type.properties");
-        props.load(in);
-        Enumeration<?> en = props.propertyNames();
-        while (en.hasMoreElements()) {
-            String key = (String) en.nextElement();
-            String property = props.getProperty(key);
-            map.put(key, property);
-            System.out.println(key + "<====>" + property);
+        try {
+            props.load(in);
+            Enumeration<?> en = props.propertyNames();
+            while (en.hasMoreElements()) {
+                String key = (String) en.nextElement();
+                String property = props.getProperty(key);
+                map.put(key, property);
+                System.out.println(key + "<====>" + property);
+            }
+        } catch (IOException e) {
         }
         System.out.println("====================================================================");
         return map;
